6e70f92b2de3537b6d2d74a6f363630925172484
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-07-17 14:01:17 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  * DPRO 放課後等デイサービス LINE
  * STEP HOUKAGO-4
  * 共通設定 config.js 完全版
- * Version: HOUKAGO-4-R5-CONFIG-OWNER-MASTER-DATA-20260717
+ * Version: HOUKAGO-4-R5-R1-CONFIG-OWNER-MASTER-COLUMN-FIX-20260717
  * ============================================================
  *
  * GitHub配置先:
@@ -27,8 +27,8 @@
     serviceName: "DPRO 放課後等デイサービス LINE",
     shortName: "DPRO放課後等デイ",
     subtitle: "保護者連絡・利用予定・送迎・日々の支援共有をLINEで一元管理",
-    appVersion: "HOUKAGO-4-R5-CONFIG-OWNER-MASTER-DATA-20260717",
-    workerVersion: "HOUKAGO-3-R5-WORKER-OWNER-MASTER-DATA-20260717",
+    appVersion: "HOUKAGO-4-R5-R1-CONFIG-OWNER-MASTER-COLUMN-FIX-20260717",
+    workerVersion: "HOUKAGO-3-R5-R1-WORKER-OWNER-MASTER-COLUMN-FIX-20260717",
     facilityCode: "dpro_houkago_demo",
     facilityName: "DPRO放課後等デイサービス",
     corporationName: "DPRO福祉サポート",
